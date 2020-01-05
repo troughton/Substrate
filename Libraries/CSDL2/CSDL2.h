18d495516b5a68c9adce46ad5f7ebaa3d4ed13a1
Joseph Bennett <service@josephbennett.me>
2020-01-05 13:45:39 +1300

    Import metal headers for SDL2.

--- Libraries/CSDL2/CSDL2.h
+++ Libraries/CSDL2/CSDL2.h
@@ -1,6 +1,7 @@
 #ifdef __APPLE__
 #include "/usr/local/include/SDL2/SDL.h"
 #include "/usr/local/include/SDL2/SDL_vulkan.h"
+#include "/usr/local/include/SDL2/SDL_metal.h"
 #else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
