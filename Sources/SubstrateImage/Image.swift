75d6140586617f0a0c88025f34bbe78406778eb8
Thomas Roughton <t.roughton@me.com>
2023-11-03 14:05:28 +1300

    Image loading: recognise the sRGB ICC profile name in PNGs

--- Sources/SubstrateImage/Image.swift
+++ Sources/SubstrateImage/Image.swift
@@ -170,7 +170,7 @@ public enum ImageColorSpace: Hashable, Sendable {
         case .linearSRGB:
             return color
         case .gammaSRGB(let gamma):
-            return pow(color, 1.0 / gamma)
+            return Float.pow(color, 1.0 / gamma)
         }
     }
     
